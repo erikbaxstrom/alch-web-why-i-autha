412f7f41cf7b0feeee4d6a243c9b547d02ef80ff
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-26 15:13:27 -0700

    add supabase calls to getUser, signUpUser, and signInUser

--- fetch-utils.js
+++ fetch-utils.js
@@ -7,14 +7,23 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export function getUser() {
     // > Part A: supabase get user
+    return client.auth.getUser();
 }
 
 export async function signUpUser(email, password) {
     // > Part A: supabase signup
+    return await client.auth.signUpUser({
+        email,
+        password,
+    });
 }
 
 export async function signInUser(email, password) {
     // > Part A: supabase signin
+    return await client.auth.signInUser({
+        email,
+        password,
+    });
 }
 
 export async function signOutUser() {
