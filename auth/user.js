78e16570054717c6d732e682d6c9ce13a44489eb
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-26 22:37:10 -0700

    implement redirects

--- auth/user.js
+++ auth/user.js
@@ -5,11 +5,18 @@ const signOutLink = document.getElementById('sign-out-link');
 // make sure we have a user!
 // > Part C:
 //     - get the user (if there is one)
+let user = getUser();
 //     - check if there is not a user and if so do the redirect
 //       redirect code: location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+if (user) {
+    console.log('got user');
+} else {
+    console.log('no user');
+    // location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+    location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+}
 
 // > Part B: attach event listener to signOutLink that calls signOutUser
-
 signOutLink.addEventListener('click', () => {
     signOutUser();
 });
